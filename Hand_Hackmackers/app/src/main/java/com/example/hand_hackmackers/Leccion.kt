90c9a9e5e1361c155dbc252bbddf6f6dabd53ec7
AlmaGordillo <almagoram@gmail.com>
2021-04-18 14:23:12 -0500

    Lessons Buttons
    Lessons

--- Hand_Hackmackers/app/src/main/java/com/example/hand_hackmackers/Leccion.kt
+++ Hand_Hackmackers/app/src/main/java/com/example/hand_hackmackers/Leccion.kt
@@ -8,6 +8,7 @@ import android.os.Bundle
 import android.view.View
 import android.view.WindowManager
 import android.widget.Button
+import android.widget.ImageView
 import android.widget.TextView
 import android.widget.Toast
 //import androidx.core.content.ContextCompat
@@ -29,10 +30,8 @@ class Leccion : AppCompatActivity() {
         //Toast.makeText(this@Leccion, "" + leccion, Toast.LENGTH_SHORT).show()
 
         val titulo = findViewById(R.id.textView2) as TextView
-        val iconoLeccAbc = findViewById(R.id.button) as Button
-        val iconoJuegoAbc = findViewById(R.id.button2) as Button
-        val iconoLeccNum = findViewById(R.id.button3) as Button
-        val iconoJuegoNum = findViewById(R.id.button4) as Button
+        val iconoLecc = findViewById(R.id.imageView9) as ImageView
+        val iconoJuego = findViewById(R.id.imageView10) as ImageView
 
         if(leccion==1)
         {
@@ -41,10 +40,8 @@ class Leccion : AppCompatActivity() {
             //iconoJuegoAbc.isVisible=true
             //iconoLeccNum.isVisible=false
             //iconoJuegoNum.isVisible=false
-            iconoLeccAbc.visibility= View.VISIBLE
-            iconoJuegoAbc.visibility = View.VISIBLE
-            iconoLeccNum.visibility = View.GONE
-            iconoJuegoNum.visibility = View.GONE
+            iconoLecc.setImageResource(R.drawable.boton_leccionra)
+            iconoJuego.setImageResource(R.drawable.boton_juegoab)
         }
         else
         {
@@ -53,43 +50,45 @@ class Leccion : AppCompatActivity() {
             //iconoJuegoAbc.isVisible=false
             //iconoLeccNum.isVisible=true
             //iconoJuegoNum.isVisible=true
-            iconoLeccAbc.visibility= View.GONE
-            iconoJuegoAbc.visibility = View.GONE
-            iconoLeccNum.visibility = View.VISIBLE
-            iconoJuegoNum.visibility = View.VISIBLE
+            iconoLecc.setImageResource(R.drawable.boton_leccionran)
+            iconoJuego.setImageResource(R.drawable.boton_juegonu)
 
         }
 
-        iconoLeccAbc.setOnClickListener {
-            val intent = Intent(this@Leccion, Leccion_AR::class.java)
-            intent.putExtra("leccion_valor", 1);
-            intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP or Intent.FLAG_ACTIVITY_SINGLE_TOP)
-            startActivity(intent)
-            //Toast.makeText(this@MainActivity, "You clicked 123", Toast.LENGTH_SHORT).show()
+        iconoLecc.setOnClickListener {
+            iconoLecc.setColorFilter(0x206a5d.toInt(), PorterDuff.Mode.MULTIPLY);
+            if(leccion==1){
+                val intent = Intent(this@Leccion, Leccion_AR::class.java)
+                intent.putExtra("leccion_valor", 1);
+                intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP or Intent.FLAG_ACTIVITY_SINGLE_TOP)
+                startActivity(intent)
+                //Toast.makeText(this@MainActivity, "You clicked 123", Toast.LENGTH_SHORT).show()
+            }
+            else{
+                val intent = Intent(this@Leccion, Leccion_AR::class.java)
+                intent.putExtra("leccion_valor", 2);
+                intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP or Intent.FLAG_ACTIVITY_SINGLE_TOP)
+                startActivity(intent)
+                //Toast.makeText(this@MainActivity, "You clicked 123", Toast.LENGTH_SHORT).show()
+            }
         }
 
-        iconoLeccNum.setOnClickListener {
-            val intent = Intent(this@Leccion, Leccion_AR::class.java)
-            intent.putExtra("leccion_valor", 2);
-            intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP or Intent.FLAG_ACTIVITY_SINGLE_TOP)
-            startActivity(intent)
-            //Toast.makeText(this@MainActivity, "You clicked 123", Toast.LENGTH_SHORT).show()
-        }
-
-        iconoJuegoAbc.setOnClickListener {
-            val intent = Intent(this@Leccion, JuegoNum::class.java)
-            intent.putExtra("leccion_valor", 1);
-            intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP or Intent.FLAG_ACTIVITY_SINGLE_TOP)
-            startActivity(intent)
-            //Toast.makeText(this@MainActivity, "You clicked 123", Toast.LENGTH_SHORT).show()
-        }
-
-        iconoJuegoNum.setOnClickListener {
-            val intent = Intent(this@Leccion, JuegoNum::class.java)
-            intent.putExtra("leccion_valor", 2);
-            intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP or Intent.FLAG_ACTIVITY_SINGLE_TOP)
-            startActivity(intent)
-            //Toast.makeText(this@MainActivity, "You clicked 123", Toast.LENGTH_SHORT).show()
+        iconoJuego.setOnClickListener {
+            iconoJuego.setColorFilter(0x206a5d.toInt(), PorterDuff.Mode.MULTIPLY);
+            if(leccion==1){
+                val intent = Intent(this@Leccion, JuegoNum::class.java)
+                intent.putExtra("leccion_valor", 1);
+                intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP or Intent.FLAG_ACTIVITY_SINGLE_TOP)
+                startActivity(intent)
+                //Toast.makeText(this@MainActivity, "You clicked 123", Toast.LENGTH_SHORT).show()
+            }
+            else{
+                val intent = Intent(this@Leccion, JuegoNum::class.java)
+                intent.putExtra("leccion_valor", 2);
+                intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP or Intent.FLAG_ACTIVITY_SINGLE_TOP)
+                startActivity(intent)
+                //Toast.makeText(this@MainActivity, "You clicked 123", Toast.LENGTH_SHORT).show()
+            }
         }
     }
 }
